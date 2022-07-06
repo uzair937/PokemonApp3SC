6aed165f00bc581f1d2b8d5de6d7101d1589e8ab
Uzair Foolat <41326453+uzair937@users.noreply.github.com>
2022-07-06 21:50:53 +0100

    Basic UI Setup

--- app/src/main/java/com/example/pokemonapp3sc/ui/randomiser/RandomiserFragment.java
+++ app/src/main/java/com/example/pokemonapp3sc/ui/randomiser/RandomiserFragment.java
@@ -1,4 +1,4 @@
-package com.example.pokemonapp3sc.ui.dashboard;
+package com.example.pokemonapp3sc.ui.randomiser;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pokemonapp3sc.databinding.FragmentDashboardBinding;
+import com.example.pokemonapp3sc.databinding.FragmentRandomiserBinding;
 
-public class DashboardFragment extends Fragment {
+public class RandomiserFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentRandomiserBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        RandomiserViewModel RandomiserViewModel =
+                new ViewModelProvider(this).get(RandomiserViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentRandomiserBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textRandomiser;
+        RandomiserViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/pokemonapp3sc/ui/randomiser/RandomiserViewModel.java
+++ app/src/main/java/com/example/pokemonapp3sc/ui/randomiser/RandomiserViewModel.java
@@ -1,16 +1,16 @@
-package com.example.pokemonapp3sc.ui.dashboard;
+package com.example.pokemonapp3sc.ui.randomiser;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class RandomiserViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public RandomiserViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is the random Pokemon fragment");
     }
 
     public LiveData<String> getText() {
