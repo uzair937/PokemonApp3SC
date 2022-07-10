57fde55c1d0fdf6f5b2a994c73be9b118a755682
Uzair Foolat <41326453+uzair937@users.noreply.github.com>
2022-07-10 23:37:50 +0100

    Home page functionality

---
+++ app/src/main/java/com/example/pokemonapp3sc/adapters/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.example.pokemonapp3sc.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class ImageAdapter extends BaseAdapter {
+    private List<Integer> thumbnailIds;
+    private Context context;
+
+    public ImageAdapter(List<Integer> thumbnailIds, Context mContext) {
+        this.thumbnailIds = thumbnailIds;
+        this.context = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return thumbnailIds.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return thumbnailIds.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = (ImageView) convertView;
+
+        if(imageView == null){
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(350,450));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }
+
+        imageView.setImageResource(thumbnailIds.get(position));
+
+        return imageView;
+    }
+}
--- app/src/main/java/com/example/pokemonapp3sc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/pokemonapp3sc/ui/home/HomeFragment.java
@@ -1,20 +1,31 @@
 package com.example.pokemonapp3sc.ui.home;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pokemonapp3sc.R;
+import com.example.pokemonapp3sc.adapters.ImageAdapter;
 import com.example.pokemonapp3sc.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private GridView pokemonGrid;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +37,42 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        ArrayList<Integer> icons = new ArrayList< >(Arrays.asList(
+                R.drawable.pokemon_logo_png_1428
+        ));
+        pokemonGrid = binding.gridView;
+        pokemonGrid.setAdapter(new ImageAdapter(icons, getActivity().getApplicationContext()));
+        pokemonGrid.setOnItemClickListener((parent, view, position, id) -> {
+            int pos = icons.get(position);
+            showDialogBox(pos);
+        });
         return root;
+    }
+
+    private void showDialogBox(final int item_pos){
+        final Dialog dialog = new Dialog(getContext());
+
+        dialog.setContentView(R.layout.grid_dialog);
+
+        //Getting custom dialog views
+        TextView Image_name = dialog.findViewById(R.id.txt_Image_name);
+        ImageView Image = dialog.findViewById(R.id.img);
+        Button btn_Close = dialog.findViewById(R.id.btn_close);
+
+        String title = getResources().getResourceName(item_pos);
+
+        //extracting name
+        int index = title.indexOf("/");
+        String name = title.substring(index+1);
+        Image_name.setText(name);
+
+        Image.setImageResource(item_pos);
+
+        btn_Close.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+
     }
 
     @Override
